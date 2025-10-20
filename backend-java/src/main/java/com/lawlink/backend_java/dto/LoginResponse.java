b475de8fa37cba08e9150f45a9cc83281892e7f0
davidfoo07 <beckhamfth@gmail.com>
2025-10-20 21:27:27 +0800

    added user id into login reponse dto

--- backend-java/src/main/java/com/lawlink/backend_java/dto/LoginResponse.java
+++ backend-java/src/main/java/com/lawlink/backend_java/dto/LoginResponse.java
@@ -2,15 +2,27 @@ package com.lawlink.backend_java.dto;
 
 import com.lawlink.backend_java.enums.Role;
 
+import java.util.UUID;
+
 public class LoginResponse {
+    private UUID userId;
     private String username;
     private String email;
     private Role role;
 
-    public LoginResponse(String username, String email, Role role) {
+    public LoginResponse(UUID userId, String username, String email, Role role) {
+        this.userId = userId;
         this.username = username;
         this.email = email;
         this.role = role;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
     }
 
     public String getUsername() {
