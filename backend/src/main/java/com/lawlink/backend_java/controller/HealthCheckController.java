a8d418f4409d5b02ef2a87e5905219dc37ef2faf
davidfoo07 <beckhamfth@gmail.com>
2025-10-27 20:27:51 +0800

    added health check controller for deployment process

---
+++ backend/src/main/java/com/lawlink/backend_java/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.lawlink.backend_java.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Application is up and running";
+    }
+}
